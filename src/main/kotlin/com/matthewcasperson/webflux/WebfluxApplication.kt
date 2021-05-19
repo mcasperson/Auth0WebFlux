a9cac9c6076929f1d0650adf2539abe575c175b6
Matthew Casperson <matthewcasperson@gmail.com>
2021-05-19 14:07:50 +1000

    Initial commit

---
+++ src/main/kotlin/com/matthewcasperson/webflux/WebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.matthewcasperson.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxApplication>(*args)
+}
